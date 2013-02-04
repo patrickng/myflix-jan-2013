ab6638f393f656e7198f420a71322bcd98dea901
Patrick Ng <ckipel@gmail.com>
2013-02-04 01:47:33 -0500

    refactoring category model spec

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,8 +1,10 @@
 class Category < ActiveRecord::Base
-  attr_accessible :description, :name
 
   has_many :categorizations
   has_many :videos, through: :categorizations
+
+  validates :description, presence: true
+  validates :name, presence: true
 
   def recent_videos
     videos.order('created_at DESC').limit(6)
