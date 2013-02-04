0a23711452123f6b4161ecba0a19f325fb28fb53
Patrick Ng <ckipel@gmail.com>
2013-02-04 01:59:48 -0500

    refactoring users controller test

--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -10,21 +10,27 @@ describe UsersController do
   end
 
   describe "POST create" do
-    it "creates the user when input is valid" do
-      post :create, user: { email_address: "test@test.com", full_name: "test tester", password: "test" }
-      User.first.email_address.should == "test@test.com"
-      User.first.full_name.should == "test tester"
-      User.first.authenticate('test').should be_true
+    context "user is saved" do
+      before(:each) do
+        post :create, user: { email_address: "test@test.com", full_name: "test tester", password: "test" }
+      end
+
+      it "creates the user when input is valid" do
+        User.first.email_address.should == "test@test.com"
+        User.first.full_name.should == "test tester"
+        User.first.authenticate('test').should be_true
+      end
+
+      it "redirects user to root when user is saved" do
+        response.should redirect_to root_path
+      end
     end
 
-    it "redirects user to root when user is saved" do
-      post :create, user: { email_address: "test@test.com", full_name: "test tester", password: "test" }
-      response.should redirect_to root_path
-    end
-
-    it "renders new template when user is not created" do
-      post :create, user: { email_address: "", full_name: "", password: "" }
-      response.should render_template :new
+    context "user is not saved" do
+      it "renders new template when user is not created" do
+        post :create, user: { email_address: "", full_name: "", password: "" }
+        response.should render_template :new
+      end
     end
   end
 end
