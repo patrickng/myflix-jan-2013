00d6d8ff2fde9e42c544ed813c154369602b75af
Patrick Ng <ckipel@gmail.com>
2013-02-04 01:46:33 -0500

    adding tests for sessions, users, and static

--- spec/controllers/sessions_controller_spec.rb
+++ spec/controllers/sessions_controller_spec.rb
@@ -1,5 +1,47 @@
 require 'spec_helper'
 
 describe SessionsController do
+  describe "GET new" do
+    it "renders the new template" do
+      get :new
+      response.should render_template :new
+    end
+  end
 
+  describe "POST create" do
+    before(:each) do
+      @user = User.create(email_address: "test@test.com", full_name: "test tester", password: "test")
+    end
+
+    it "sets the session user" do
+      post :create, { email_address: "test@test.com", password: "test" }
+      session[:user].should == @user
+    end
+
+    it "redirects to home path when login is successful" do
+      post :create, { email_address: "test@test.com", password: "test" }
+      response.should redirect_to home_path
+    end
+
+    it "redirects to login path when login is unsuccessful" do
+      post :create, { email_address: "test@test.com", password: "testing" }
+      response.should redirect_to login_path
+    end
+  end
+
+  describe "GET destroy" do
+    before(:each) do
+      @user = User.create(email_address: "test@test.com", full_name: "test tester", password: "test")
+      post :create, { email_address: "test@test.com", password: "test" }
+      get :destroy
+    end
+
+    it "empties the session when user logs out" do
+      session[:user].should == nil
+    end
+
+    it "redirects to root path when user logs out" do
+      response.should redirect_to root_path
+    end
+  end
 end
--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -1,5 +1,30 @@
 require 'spec_helper'
 
 describe UsersController do
+  describe "GET new" do
+    it "sets the @user variable" do
+      get :new
+      assigns(:user).should be_new_record
+      assigns(:user).should be_instance_of(User)
+    end
+  end
 
+  describe "POST create" do
+    it "creates the user when input is valid" do
+      post :create, user: { email_address: "test@test.com", full_name: "test tester", password: "test" }
+      User.first.email_address.should == "test@test.com"
+      User.first.full_name.should == "test tester"
+      User.first.authenticate('test').should be_true
+    end
+
+    it "redirects user to root when user is saved" do
+      post :create, user: { email_address: "test@test.com", full_name: "test tester", password: "test" }
+      response.should redirect_to root_path
+    end
+
+    it "renders new template when user is not created" do
+      post :create, user: { email_address: "", full_name: "", password: "" }
+      response.should render_template :new
+    end
+  end
 end
