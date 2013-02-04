b0ba735c044f28cf9ffa559295c8457bd0102a21
Patrick Ng <ckipel@gmail.com>
2013-02-04 10:38:31 -0500

    refactoring user controller spec

--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -15,18 +15,25 @@ describe UsersController do
         post :create, user: { email_address: "test@test.com", full_name: "test tester", password: "test" }
       end
 
-      it "creates the user when input is valid" do
-        User.first.email_address.should == "test@test.com"
-        User.first.full_name.should == "test tester"
-        User.first.authenticate('test').should be_true
-      end
+      context "user is saved" do
+        it "creates the user successfully" do
+          User.first.email_address.should == "test@test.com"
+          User.first.full_name.should == "test tester"
+          User.first.authenticate('test').should be_true
+        end
 
-      it "redirects user to root when user is saved" do
-        response.should redirect_to root_path
+        it "redirects user to root" do
+          response.should redirect_to root_path
+        end
       end
     end
 
     context "user is not saved" do
+      it "does not create the account" do
+        post :create, user: { email_address: "test@test.com" }
+        User.all.count.should == 0
+      end
+
       it "renders new template when user is not created" do
         post :create, user: { email_address: "", full_name: "", password: "" }
         response.should render_template :new
