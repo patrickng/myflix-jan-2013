d67f80be19b6b831f7932dd3734cc2d741d9b10f
Patrick Ng <ckipel@gmail.com>
2013-02-04 10:24:18 -0500

    updating user model for uniqueness on email field

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,6 @@ class User < ActiveRecord::Base
   has_secure_password
 
   validates :full_name, presence: true
-  validates :email_address, presence: true
+  validates :email_address, presence: true, uniqueness: true
   validates :password, presence: true
 end
