572f1c57de70ee6456ff926c92e494d83c6ffe4f
Patrick Ng <ckipel@gmail.com>
2013-02-08 03:24:47 -0500

    initial implementation of the rating drop down on my queue

--- app/controllers/queue_items_controller.rb
+++ app/controllers/queue_items_controller.rb
@@ -1,7 +1,11 @@
 class QueueItemsController < ApplicationController
   before_filter :require_user
-  
+
   def index
     @queue = QueueItem.find_all_by_user_id(session[:user_id])
+  end
+
+  def create
+
   end
 end
