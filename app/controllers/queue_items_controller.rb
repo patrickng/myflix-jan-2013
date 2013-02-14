a62e5c93a1420cc5cb349ef2e82128c32c245084
Patrick Ng <ckipel@gmail.com>
2013-02-14 16:14:32 -0500

    hw 6.3

--- app/controllers/queue_items_controller.rb
+++ app/controllers/queue_items_controller.rb
@@ -18,18 +18,17 @@ class QueueItemsController < ApplicationController
     end
   end
 
+  def update
+    if QueueItem.reorder(params[:queue_items])
+      redirect_to my_queue_path
+    else
+      redirect_to home_path, flash: { error: "You have no videos in your queue." }
+    end
+  end
+
   def destroy
     queue_item = current_user.queue_items.find(params[:id])
     queue_item.destroy
-
-    redirect_to my_queue_path
-  end
-
-  def sort
-    sorted = params[:queue_items].sort_by { |key, value| value['position'] }
-    sorted.each_with_index do |item, index|
-      QueueItem.find(item[0].to_i).update_attributes(position: index + 1)
-  end
 
     redirect_to my_queue_path
   end
--- app/models/queue_item.rb
+++ app/models/queue_item.rb
@@ -1,11 +1,18 @@
 class QueueItem < ActiveRecord::Base
-  # attr_accessible :category, :instant_queue_id, :video_rating, :video_title
-
   belongs_to :user
   belongs_to :video
 
   def user_rating
     review = video.reviews.where(user_id: user_id).first
     review.nil? ? '' : review.rating.to_i
+  end
+
+  def self.reorder(queue_items)
+    unless queue_items.nil?
+      sorted = queue_items.sort_by { |key, value| value['position'] }
+      sorted.each_with_index do |item, index|
+        self.find(item[0].to_i).update_attributes(position: index + 1)
+      end
+    end
   end
 end
