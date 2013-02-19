576c86c841808d4daaf3c61f3c5bfe12e612ed09
Patrick Ng <ckipel@gmail.com>
2013-02-18 19:36:26 -0500

    adding fail test to user log in

--- spec/feature/user_log_in_spec.rb
+++ spec/feature/user_log_in_spec.rb
@@ -6,11 +6,19 @@ feature "User log in" do
     User.create(full_name: "Patrick Example", email_address: "patrick@example.me", password: "test88")
   end
 
-  scenario "with existing username and password" do
+  scenario "with existing email address and password" do
     visit login_path
     fill_in "Email Address:", with: "patrick@example.me"
     fill_in "Password", with: "test88"
     click_button "Sign in"
     page.should have_content "Successfully logged in!"
+  end
+
+  scenario "with incorrect email address and password" do
+    visit login_path
+    fill_in "Email Address:", with: "patrik@example.me"
+    fill_in "Password", with: "test89"
+    click_button "Sign in"
+    page.should have_content "Invalid email or password. Please try again."
   end
 end
