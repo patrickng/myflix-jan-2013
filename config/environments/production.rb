bb615725bc9651e21daf08912e5bc14fa0b41451
Patrick Ng <ckipel@gmail.com>
2013-03-03 03:26:52 -0500

    adding single quotations to ENV vars

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -69,8 +69,8 @@ Myflix::Application.configure do
     :address              => "smtp.gmail.com",
     :port                 => 587,
     :domain               => 'mighty-anchorage-4285.herokuapp.com',
-    :user_name            => ENV[GMAIL_USERNAME],
-    :password             => ENV[GMAIL_PASSWORD],
+    :user_name            => ENV['GMAIL_USERNAME'],
+    :password             => ENV['GMAIL_PASSWORD'],
     :authentication       => 'plain',
     :enable_starttls_auto => true  }
 end
