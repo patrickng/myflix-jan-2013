2b24ac0f4ee499c9c1f7645ccf7a8d0da644dd4d
Patrick Ng <ckipel@gmail.com>
2013-03-05 03:18:55 -0500

    removing unnecessary generation of tokens

--- db/migrate/20130304002708_add_password_reset_to_users.rb
+++ db/migrate/20130304002708_add_password_reset_to_users.rb
@@ -2,10 +2,5 @@ class AddPasswordResetToUsers < ActiveRecord::Migration
   def change
     add_column :users, :password_reset_token, :string
     add_column :users, :password_reset_sent_at, :datetime
-
-    User.all.each do |user|
-      user.generate_token
-      user.save(validate: false)
-    end
   end
 end
