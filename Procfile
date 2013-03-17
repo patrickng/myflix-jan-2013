9a853484a44ca430c728da79ab227c0392ada2f4
Patrick Ng <ckipel@gmail.com>
2013-03-17 05:56:24 -0400

    updating procfile and config/unicorn.rb

--- Procfile
+++ Procfile
@@ -1,2 +1,2 @@
-web: bundle exec unicorn
+web: bundle exec unicorn_rails -p $PORT -c ./config/unicorn.rb
 worker: bundle exec sidekiq
--- config/unicorn.rb
+++ config/unicorn.rb
@@ -1,7 +1,29 @@
+# config/unicorn.rb
 if ENV["RAILS_ENV"] == "development"
-  worker_processes 1
+  worker_processes Integer(ENV["WEB_CONCURRENCY"] || 1)
 else
-  worker_processes 3
+  worker_processes Integer(ENV["WEB_CONCURRENCY"] || 3)
 end
+timeout 15
+preload_app true
 
-timeout 30
+before_fork do |server, worker|
+
+  Signal.trap 'TERM' do
+    puts 'Unicorn master intercepting TERM and sending myself QUIT instead'
+    Process.kill 'QUIT', Process.pid
+  end
+
+  defined?(ActiveRecord::Base) and
+    ActiveRecord::Base.connection.disconnect!
+end  
+
+after_fork do |server, worker|
+
+  Signal.trap 'TERM' do
+    puts 'Unicorn worker intercepting TERM and doing nothing. Wait for master to send QUIT'
+  end
+
+  defined?(ActiveRecord::Base) and
+    ActiveRecord::Base.establish_connection
+end
