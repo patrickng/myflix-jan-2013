224200dda537ae47ac3f20873fe48183825c0e00
Patrick Ng <ckipel@gmail.com>
2013-03-22 19:38:03 -0400

    hw 12.3 sans tests

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130322071806) do
+ActiveRecord::Schema.define(:version => 20130322233515) do
 
   create_table "categories", :force => true do |t|
     t.string   "name"
@@ -87,6 +87,7 @@ ActiveRecord::Schema.define(:version => 20130322071806) do
     t.string   "large_cover"
     t.datetime "created_at",  :null => false
     t.datetime "updated_at",  :null => false
+    t.string   "url"
   end
 
 end
