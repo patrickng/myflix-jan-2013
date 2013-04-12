bd30f0b127fc91371a1782d1b207a81f2685c8b8
Patrick Ng <ckipel@gmail.com>
2013-04-12 02:05:52 -0400

    Revert "attempting to move logic from controller to service object (getting error)"
    This reverts commit 062d8192671873a2a3c3f93ec89ac451a5cdd56b.

--- app/services/user_registration.rb
+++ app/services/user_registration.rb
@@ -1,33 +1,14 @@
 class UserRegistration
-  include AbstractController::Rendering
-  attr_reader :invitation, :token
-  attr_accessor :user
+  attr_accessor :user, :invitation
 
-  def initialize(user, invitation, token)
+  def initialize(user, invitation)
     @user = user
     @invitation = invitation
   end
 
   def process_registration
-    if user.valid?
-      charge = StripeGateway::Charge.create(amount: 999, card: token)
-      if charge.successful?
-        flash[:success] = "Thank you for your payment."
-        if user.save
-          # UserRegistration.new(@user, @user.invitation).process_registration
-          UserMailer.delay.welcome_email(user.id)
+    UserMailer.delay.welcome_email(user.id)
     handle_invitation if invitation
-          redirect_to login_path, flash: { notice: "You have successfully signed up. Please sign in." }
-        else
-          render 'new'
-        end
-      else
-        flash[:error] = charge.error_message
-        render 'new'
-      end
-    else
-      render 'new'
-    end
   end
 
   private
